1cb040e345b7ed5f064a855d848be6265ddafebf
AkindeleRoqeeb <roqeebakindele@gmail.com>
2023-12-28 23:04:37 +0100

    python

---
+++ salary_Cal.py
@@ -0,0 +1,9 @@
+print("pay salary to worker easily enterpren")
+
+total_amount = input("please enter the total amount here? ")
+num_of_people = input("please enter the number of people to share with ? ")
+percentage = input("what would like to be your percentage ? ")
+
+result = total_amount / num_of_people * percentage
+
+print(result)
